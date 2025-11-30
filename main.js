b97b737f83d3a4396430f74139a905914637147e
samko12 <55701948+samko12@users.noreply.github.com>
2025-11-30 21:05:15 +0100

    Add files via upload

--- main.js
+++ main.js
@@ -1,35 +1,43 @@
+const dtoIn = {
+  count: 6,
+  age: {
+    min: 19,
+    max: 35,
+  },
+};
+const names = [
+  "Jan","Petr","Pavel","Martin","Tomáš","Jakub","Lukáš","Jiří","David","Josef",
+  "Adam","Matěj","Filip","Václav","Daniel","Marek","Jaroslav","Štěpán","Ondřej","Karel",
+  "Vojtěch","Michal","Roman","Zdeněk","Radek","Oldřich","Robert","Aleš","Milan","Richard",
+  "Tereza","Anna","Eliška","Karolína","Adéla","Kristýna","Natálie","Veronika","Markéta","Barbora",
+  "Lucie","Klára","Kateřina","Nikola","Monika","Gabriela","Simona","Alena","Iveta","Jana"
+];
+const surnames = [
+  "Novák","Svoboda","Novotný","Dvořák","Černý","Procházka","Kučera","Veselý","Horák","Němec",
+  "Mareš","Pospíšil","Hájek","Jelínek","Král","Růžička","Beneš","Fiala","Sedláček","Doležal",
+  "Zeman","Urban","Vaněk","Kolář","Štěpánek","Polák","Kříž","Pokorný","Konečný","Malý",
+  "Kovář","Bláha","Strnad","Holý","Soukup","Matoušek","Tichý","Hlaváček","Kočí","Bečka",
+  "Suchý","Zajíček","Pazdera","Leoš","Staňek","Burda","Mašek","Čížek","Stehlík","Gregor"
+];
+const gender = ["male", "female"];
+const workload = [10, 20, 30, 40];
 export function main(dtoIn) {
   const dtoOut = [];
   const count = dtoIn.count;
   const currentYear = new Date().getFullYear();
-
-  const usedDates = new Set(); // kvôli unikátnosti dátumov
-
   for (let i = 0; i < count; i++) {
-
-    // vek
     const randomAge = Math.floor(Math.random() * (dtoIn.age.max - dtoIn.age.min + 1)) + dtoIn.age.min;
     const birthYear = currentYear - randomAge;
-
-    let birthdate;
-
-    // unikátne dátumy
-    do {
-      const month = String(Math.floor(Math.random() * 12) + 1).padStart(2, "0");
-      const day = String(Math.floor(Math.random() * 28) + 1).padStart(2, "0");
-      birthdate = `${birthYear}-${month}-${day}T00:00:00.000Z`;
-    } while (usedDates.has(birthdate));
-
-    usedDates.add(birthdate);
-
+    const month = String(Math.floor(Math.random() * 12) + 1).padStart(2, "0");
+    const day = String(Math.floor(Math.random() * 28) + 1).padStart(2, "0");
+    const birthdate = `${birthYear}-${month}-${day}T00:00:00.000Z`;
     dtoOut.push({
       name: names[Math.floor(Math.random() * names.length)],
       surname: surnames[Math.floor(Math.random() * surnames.length)],
       gender: gender[Math.floor(Math.random() * gender.length)],
       workload: workload[Math.floor(Math.random() * workload.length)],
-      birthdate
+      birthdate: birthdate,
     });
   }
-
   return dtoOut;
 }
